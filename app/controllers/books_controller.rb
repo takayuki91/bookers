fa6e13852d7f202c018d8a5ff0c4c962946fc480
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-04-11 05:31:03 +0000

    [Add]rspec

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,15 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     if @book.save
        flash[:notice] = "Book was successfully created!!"
-       redirect_to books_path
+       redirect_to book_path(@book.id)
     else
        @books = Book.all
        render :index
@@ -23,7 +23,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -33,16 +33,16 @@ class BooksController < ApplicationController
        render :edit
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
